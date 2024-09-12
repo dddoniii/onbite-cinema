0827d2badfa8d15ce9d32553cd58ab469774a67c
dddoniii <eunjin9487@gmail.com>
2024-09-12 22:24:15 +0900

    mission_day03(글로벌 레이아웃 설정, 검색바 레이아웃 설정)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import GlobalLayout from "./components/global-layout";
+import { NextPage } from "next";
+import { ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+type NextPageLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & { Component: NextPageLayout }) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+
+  return <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>;
 }
